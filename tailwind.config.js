c2165689d397165f51c2c8ba58dac9bc7a7e16b7
Sarosh Farhan <saroshfarhan6495@gmail.com>
2022-11-26 13:51:33 +0530

    tailwind css setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        transparent: "transparent",
+        current: "currentColor",
+        lightGray: "hsl(212, 45%, 89%)",
+        grayishBlue: "hsl(220, 15%, 55%)",
+        darkBlue: "hsl(218, 44%, 22%)",
+      },
+    },
+    container: {
+      center: true,
+    },
   },
   plugins: [],
 };
